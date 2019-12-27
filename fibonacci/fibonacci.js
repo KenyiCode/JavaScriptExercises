010c9cfa6427c900e07d915208a9f68f61adb830
Kenyiro Anazagasty <kenyicode@gmail.com>
2019-12-26 20:56:43 -0500

    Completed fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0 || !Number.parseInt(index)) return "OOPS";
+    
+    let fibArr = [1, 1];
 
+    for (let i = fibArr.length; i < index; i++){
+        let nextValue = fibArr[i-1] + (fibArr[i-2]);
+        fibArr.push(nextValue);
+    }
+
+    return fibArr[index - 1];
 }
 
 module.exports = fibonacci
