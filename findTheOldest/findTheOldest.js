925615652b7b1c3f55e46cf011734da4db39c679
Kenyiro Anazagasty <kenyicode@gmail.com>
2019-12-26 23:24:04 -0500

    Completed fTO exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) { 
+    let highest = 0;
+    let highestIndex = 0;
 
+    for (let i = 0; i < people.length; i++){
+        let today = new Date();
+
+        if (!people[i].yearOfDeath){
+            people[i].yearOfDeath = today.getFullYear();
+        }
+
+        if ((people[i].yearOfDeath - people[i].yearOfBirth) > highest){
+            highest = people[i].yearOfDeath - people[i].yearOfBirth;
+            highestIndex = i;
+        }
+    }
+
+    return people[highestIndex];
 }
 
 module.exports = findTheOldest
